204062b213b6c66ee6b8a39b2d83d653c87592f8
Abdul Ahad <ahmmedsumon087@gmai.com>
2022-12-03 22:01:41 +0600

    first container done.

---
+++ src/components/Header/404/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const 404 = () => {
+       return (
+              <div>
+                     <h1>Upps!! SORRY</h1>
+                     <h3>This page is not available!</h3>
+              </div>
+       );
+};
+
+export default 404;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Images from '../Images/ktm.jpg';
 
 const Home = () => {
        return (
-              <div>
-                     <h1>This is home component</h1>
+              <div class="container mx-auto">
+                     <div className='grid grid-cols-2 flex justify-between'>
+                            <div className=''>
+                                   <h1 className='text-7xl font-sans'>
+                                          I do not need any weapon, <br /> I own a KTM RC✌
+                                   </h1>
+                                   <h4>
+                                   Happiness is the smell of a new car. Girlfriend must be HOT, Beer must be Chilled and Bike must be a KTM. Four wheels move the body, Two wheels move the soul. Faster, faster, faster, until the thrill of speed overcomes the fear of death.
+                                   </h4>
+                                   <a class="hire" target="_blank" href="https://www.linkedin.com/in/me-abdulahad/">HIRE ME</a>
+                            </div>
+                            <div>
+                                   <img src={Images} alt="" />
+                            </div>
+                     </div>
               </div>
        );
 };
